5bc2c6392783c57658ce981982ad8eed82574bcc
mariusz6892 <mariusz6892@gmail.com>
2018-09-05 09:26:56 +0200

    9/5/2018

---
+++ source/repos/Server Communicator/Server Communicator/Client.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server_Communicator
+{
+    class Client
+    {
+        Program program;
+        public TcpClient client;
+        public NetworkStream strumien;
+        public SslStream ssl; //szyfrowany kanał SSL
+        public BinaryReader br;
+        public BinaryWriter bw;
+
+        public Client(Program p, TcpClient c)
+        {
+            program = p;
+            client = c;
+            new Thread(new ThreadStart(SetupConnection)).Start();
+        }
+
+        void SetupConnection()
+        {
+            strumien = client.GetStream();
+            ssl = new SslStream(strumien, false);
+            ssl.AuthenticateAsServer(program.certyfikat, false, System.Security.Authentication.SslProtocols.Tls12, true);
+            br = new BinaryReader(ssl, Encoding.UTF8);
+            bw = new BinaryWriter(ssl, Encoding.UTF8);
+        }
+
+        void CloseConnection()
+        {
+            br.Close();
+            bw.Close();
+            ssl.Close();
+            strumien.Close();
+            client.Close();
+        }
+
+        public static bool ValidateCert(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            return true; //Dla niezaufanych certifikatow
+        }
+    }
+}
---
+++ source/repos/Server Communicator/Server Communicator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Server_Communicator
+{
+    class Program
+    {
+        public IPAddress ip = IPAddress.Parse("127.0.0.1");
+        public int port = 1234;
+        public bool dziala = true;
+        public TcpListener server; //TCP server
+        public X509Certificate2 certyfikat = new X509Certificate2("server.pfx", "admin");
+
+
+
+        public Program()
+        {
+            Console.Title = "Bezpieczny Komunikator Server";
+            Console.WriteLine("------ Bezpieczny Komunikator Server -----");
+            server = new TcpListener(ip, port);
+            server.Start();
+        }
+
+        void Listen() //Nadchodzące połączenia
+        {
+            while (dziala)
+            {
+                TcpClient tcpClient = server.AcceptTcpClient();
+                Client client = new Client(this, tcpClient);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine();
+            Console.WriteLine("Wciśnij Enter, aby zakończyć działanie programu.");
+            Console.ReadLine();
+        }
+    }
+}
