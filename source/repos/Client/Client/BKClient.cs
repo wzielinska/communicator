03e653997a81d2137d898ac23ddcdb7f95332684
mariusz6892 <mariusz6892@gmail.com>
2018-09-06 14:35:05 +0200

    9/6/2018

--- source/repos/Client/Client/BKClient.cs
+++ source/repos/Client/Client/BKClient.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -16,7 +20,59 @@ namespace Client
         string _username;
         string _password;
         bool _registering = false;
-        
+
+        public TcpClient client;
+        public NetworkStream strumien;
+        public SslStream ssl; //szyfrowany kanał SSL
+        public BinaryReader br;
+        public BinaryWriter bw;
+
+
+        //Events 
+
+        public event EventHandler LoginOK;
+        public event EventHandler RegisterOK;
+        public event ErrorEventHandler LoginFailed;
+        public event ErrorEventHandler RegisterFailed;
+        public event EventHandler Disconnected;
+        public event AvailableEventHandler UserAvailable;
+        public event ReceivedEventHandler MessageReceived;
+
+        protected void OnLoginOK()
+        {
+            if (LoginOK != null) LoginOK(this, EventArgs.Empty);
+        }
+
+        protected void OnRegisterOK()
+        {
+            if (RegisterOK != null) RegisterOK(this, EventArgs.Empty);
+        }
+
+        protected void OnLoginFailed(ErrorEventArgs e)
+        {
+            if (LoginFailed != null) LoginFailed(this, e);
+        }
+
+        protected void OnRegisterFailed(ErrorEventArgs e)
+        {
+            if (RegisterFailed != null) RegisterFailed(this, e);
+        }
+
+        protected void OnDisconnectedOK()
+        {
+            if (Disconnected != null) Disconnected(this, EventArgs.Empty);
+        }
+
+        protected void OnUserAvailable(AvailableEventArgs e)
+        {
+            if (UserAvailable != null) UserAvailable(this, e);
+        }
+
+        protected void OnMessageReceived(ReceivedEventArgs e)
+        {
+            if (MessageReceived != null) MessageReceived(this, e);
+        }
+
         public string Server
         {
             get { return "localhost"; }
@@ -40,12 +96,46 @@ namespace Client
 
         void SetupConnection()
         {
+            client = new TcpClient(Server, Port);
+            strumien = client.GetStream();
+            ssl = new SslStream(strumien, false, new RemoteCertificateValidationCallback(ValidateCert));
+            ssl.AuthenticateAsClient("");
+            br = new BinaryReader(ssl, Encoding.UTF8);
+            bw = new BinaryWriter(ssl, Encoding.UTF8);
+            int hello = br.ReadInt32();
+            if (hello != Packets.Hello)
+            {
+                //Zamkniecie polaczenia, wyjatek (niespodziewany pakiet)
+            }
+            bw.Write(Packets.Hello);
+            bw.Flush();
+            bw.Write(_registering ? Packets.Register : Packets.Login);
+            bw.Write(Username);
+            bw.Write(Password);
+            bw.Flush();
+            byte packet = br.ReadByte();
+            if(packet == Packets.Done)
+            {
+                if (_registering) OnRegisterOK();
+                OnLoginOK();
+            }
+            else
+            {
+                ErrorEventArgs err = new ErrorEventArgs((ErrorEvent)packet);
+                if (_registering) OnRegisterFailed(err);
+                else OnLoginFailed(err);
+            }
 
+            CloseConnection();
         }
 
         void CloseConnection()
         {
-
+            br.Close();
+            bw.Close();
+            ssl.Close();
+            strumien.Close();
+            client.Close();
         }
 
         void Connect(string username, string password, bool register){
@@ -77,6 +167,30 @@ namespace Client
             {
                 CloseConnection();
             }
+        }
+
+        public static bool ValidateCert(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            return true; //Dla niezaufanych certifikatow
+        }
+
+        void Receiver()
+        {
+            _logged = true;
+
+            try
+            {
+                while (client.Connected)
+                {
+                    byte type = br.ReadByte();
+                }
+            }
+            catch (IOException)
+            {
+
+            }
+
+            _logged = false;
         }
     }
 }
--- source/repos/Server Communicator/Server Communicator/Program.cs
+++ source/repos/Server Communicator/Server Communicator/Program.cs
@@ -9,6 +9,19 @@ using System.Security.Cryptography.X509Certificates;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 
+
+/*TODO:
+    Sprawdzanie dostepnosci innych klientow.
+    Wysylanie wiadomosci do wybranych klientow jezeli sa dostepni.
+    UI dla aplikacji klienta.
+    Szyfrowanie plikow serwera odnosnie kont (aes??)
+    Przechowywanie hasel w formie haszow (SHA-3??)
+    Książka kontaktów
+    Opcjonalnie: Xamarin, wysyłanie plików, streaming video (szyfrowanie strumieniowe??)
+   Pomysły:
+    Zamiast nazwy użytkownika stworzyć identyfikatory przydzielane odgórnie (nr telefonu coś w tym stylu)
+    Być może jakaś opcja konferencji (Stworzenie jakby grup użytkowników, do których są adresowane wiadomości czyli jeden do wielu)
+*/
 namespace Server_Communicator
 {
     class Program
@@ -51,6 +64,7 @@ namespace Server_Communicator
                 FileStream File = new FileStream(usersFileName, FileMode.Create, FileAccess.Write);
                 bf.Serialize(File, users.Values.ToArray());
                 File.Close();
+                //TODO szyfrowanie serwera pliku
             }
             catch (Exception e)
             {
@@ -66,6 +80,7 @@ namespace Server_Communicator
                 FileStream File = new FileStream(usersFileName, FileMode.Open, FileAccess.Read);
                 User[] userinfo = (User[])bf.Deserialize(File);
                 users = userinfo.ToDictionary((u) => u.Username, (u) => u); // konwersja tablicy do Dictionary
+                //TODO odszyfrowanie pliku serweera
             }
             catch
             {
